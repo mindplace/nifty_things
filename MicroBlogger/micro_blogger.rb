542d689bdc428b257c3a183803068935954f80da
Esther Leytush <eleytush@gmail.com>
2016-02-13 22:53:25 -0500

    add quit, tweet, and direct message commands

--- MicroBlogger/micro_blogger.rb
+++ MicroBlogger/micro_blogger.rb
@@ -7,19 +7,43 @@ class MicroBlogger
     @client = JumpstartAuth.twitter
   end
 
-  def tweet(message)
+  def tweet
+    puts "\nWhat would you like to tweet?"
+    print ">  "
+    message = gets.chomp
+
     if message.length < 140
       @client.update(message)
       puts "Tweet posted!"
     else raise "Tweet too long to post"
     end
   end
+
+  def direct_message
+    puts "stuff"
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   blogger = MicroBlogger.new
-  puts "What would you like to tweet?"
-  print ">  "
-  message = gets.chomp
-  blogger.tweet(message)
+  puts "\nWelcome to the JSL Twitter client!"
+  puts "Available commands:"
+  puts "   q => quit"
+  puts "   dm => direct message "
+  puts "   t => tweet\n\n"
+  command = ""
+  commands = {"dm" => "direct_message", "t"=>"tweet", "q"=>"quit"}
+  while command != "quit"
+    print "Enter command:  "
+    command = commands[gets.chomp]
+    if blogger.respond_to?(command)
+      blogger.send(command)
+      puts
+    elsif command == "quit"
+      break
+    else
+      puts "Sorry, that command is not set!"
+    end
+  end
+  puts "\nThanks for using the JSL Twitter client!\n\n"
 end
